5e5f31c957170a13be1a8733cf5b0bd6518ca478
Chefrel Baracol <chefrelbarac@gmail.com>
2026-08-23 09:52:43 +0800

    photo print bug fixed

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const PREFIX = "iconprint-cache";
-const CACHE_NAME = PREFIX + "-v2.0.20";
+const CACHE_NAME = PREFIX + "-v2.0.21";
 const PRECACHE_URLS = ["./"];
 
 // Local print/scan agent — never intercept or cache these.
