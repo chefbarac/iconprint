c039558945e982d108204f13a25006a31d289b86
Chefrel Baracol <chefrelbarac@gmail.com>
2026-08-26 17:27:17 +0800

    stickerepeat auto maintain aspect ratio

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const PREFIX = "iconprint-cache";
-const CACHE_NAME = PREFIX + "-v2.0.34";
+const CACHE_NAME = PREFIX + "-v2.0.35";
 const PRECACHE_URLS = ["./"];
 
 // Local print/scan agent — never intercept or cache these.
