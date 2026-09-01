11170c8ec6aa73d735dc97b9c10f34755460eba2
Chefrel Baracol <chefrelbarac@gmail.com>
2026-09-01 15:26:35 +0800

    photoprint add Add All upload design button

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const PREFIX = "iconprint-cache";
-const CACHE_NAME = PREFIX + "-v2.1.4";
+const CACHE_NAME = PREFIX + "-v2.1.5";
 const PRECACHE_URLS = ["./"];
 
 // Local print/scan agent — never intercept or cache these.
