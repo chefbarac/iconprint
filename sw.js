2b5ae80c8d6284728c4392db8202101753507e19
Chefrel Baracol <chefrelbarac@gmail.com>
2026-09-02 13:42:07 +0800

    photoprint added rotate all image sources

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const PREFIX = "iconprint-cache";
-const CACHE_NAME = PREFIX + "-v2.1.14";
+const CACHE_NAME = PREFIX + "-v2.1.15";
 const PRECACHE_URLS = ["./"];
 
 // Local print/scan agent — never intercept or cache these.
