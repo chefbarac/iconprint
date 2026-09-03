fd541365b7fba89ccff4ec90d30285195c4daa22
Chefrel Baracol <chefrelbarac@gmail.com>
2026-09-03 12:51:12 +0800

    photoprint allow delete of image when frames exists

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const PREFIX = "iconprint-cache";
-const CACHE_NAME = PREFIX + "-v2.1.17";
+const CACHE_NAME = PREFIX + "-v2.1.18";
 const PRECACHE_URLS = ["./"];
 
 // Local print/scan agent — never intercept or cache these.
